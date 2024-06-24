1353225b378dc140c7703b863eac7aa43ac5f778
Daniel <danielhhartmann@gmail.com>
2024-06-24 15:04:57 -0300

    a

--- keepInventory/src/main/java/com/grupo6/keepInventory/Repository/AcaoRepository.java
+++ keepInventory/src/main/java/com/grupo6/keepInventory/Repository/AcaoRepository.java
@@ -11,7 +11,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.List;
 
 public interface AcaoRepository extends JpaRepository<Acao, Long>, PagingAndSortingRepository<Acao,Long> {
-    @Query(value = "SELECT * FROM acao where Item_id = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM Acao where Item_id = ?1", nativeQuery = true)
     Page<Acao> findByItem(Long idItem, Pageable pageable);
 
     List<Acao> findByItemId(Long id);
